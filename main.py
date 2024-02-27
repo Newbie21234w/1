0a83c6802156a244ce132ccb0e44e9e060d54be8
Newbie21234w <157557621+Newbie21234w@users.noreply.github.com>
2024-02-26 19:54:07 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 result = []
+
 def divider(a, b):
- if a < b:
-   raise ValueError
- if b > 100:
-   raise IndexError
- return a/b
-data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8 : 4}
+if a < b:
+raise ValueError
+if b > 100:
+raise IndexError
+return a / b
+
+data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8: 4}
+
 for key in data:
-  res = divider(key, data[key])
-  result.append(res)
+try:
+res = divider(key, data[key])
+result.append(res)
+except (ValueError, IndexError):
+result.append(None)  # Handle exceptions by appending None to the result
 
 print(result)
